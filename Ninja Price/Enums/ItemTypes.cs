65f3cbb2dbbb765b49322599a02142b9e8f4d59f
0xE0D59 <0xE0D59@gmail.com>
2020-07-13 16:00:00 +0200

    Add Harvest seed prices

--- Ninja Price/Enums/ItemTypes.cs
+++ Ninja Price/Enums/ItemTypes.cs
@@ -22,5 +22,6 @@
         Oil,
         Catalyst,
         DeliriumOrbs,
+        HarvestSeeds,
     }
 }
--- Ninja Price/Main/CustomItem.cs
+++ Ninja Price/Main/CustomItem.cs
@@ -222,6 +222,10 @@ namespace Ninja_Price.Main
             {
                 ItemType = ItemTypes.DeliriumOrbs;
             }
+            else if (ClassName == "HarvestSeed")
+            {
+                ItemType = ItemTypes.HarvestSeeds;
+            }
             else switch (Rarity) // Unique information
             {
                 case ItemRarity.Unique when IsIdentified && IsIdentified && ClassName == "Amulet" || ClassName == "Ring" || ClassName == "Belt":
